6e54e826100086fb8e82d17b4e375bdf1ebc09a2
andrea23488 <a.ischiboni@gmail.com>
2026-09-04 22:16:41 +0000

    Fix Apple Watch target name

--- targets/watch/expo-target.config.js
+++ targets/watch/expo-target.config.js
@@ -1,7 +1,8 @@
 /** @type {import('@bacons/apple-targets/app.plugin').ConfigFunction} */
 module.exports = config => ({
   type: "watch",
-  name: "Vigilanza GPG",
+  name: "VigilanzaGPG",
+  displayName: "Vigilanza GPG",
   icon: "./icon.png",
   colors: { $accent: "darkcyan" },
   deploymentTarget: "9.4",
